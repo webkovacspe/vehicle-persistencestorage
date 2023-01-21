702838daf628fcd87617f69db006f3324cea5197
webkovacs <webkovacs@gmail.com>
2023-01-21 13:47:09 +0100

    getVehicle

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclepersistencestorage/PersistenceStorageCSV.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehiclepersistencestorage/PersistenceStorageCSV.java
@@ -6,44 +6,55 @@ import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleStorageInterface;
 import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class PersistenceStorage implements VehicleStorageInterface {
-    private Path fileName = Path.of("./vehicleStorage.csv");
+public class PersistenceStorageCSV implements VehicleStorageInterface {
+    private String fileName;
+    private Map<String, VehicleEntity> allVehicle = new HashMap<String, VehicleEntity>();
+    public PersistenceStorageCSV() {
+        this.fileName = "./vehicleStorage.csv";
+    }
     @Override
     public void saveVehicle(VehicleEntity vehicle) {
 
         String line = vehicleEntityToString(vehicle);
 
         try {
-            Files.writeString(fileName, line);
+            Files.writeString(Path.of(fileName), line);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
-
     @Override
     public VehicleEntity getVehicle(String registrationNumber) {
         loadCSV();
-        List<VehicleEntity> allVehicle = new ArrayList<VehicleEntity>();
-
-        return null;
+        return allVehicle.get(registrationNumber);
     }
-
     private void loadCSV() {
         try {
             String line = "";
             BufferedReader br = new BufferedReader(new FileReader("./vehicleStorage.csv"));
             while ((line = br.readLine()) != null) {
-                String[] vehicleStr = line.split(",");
+                VehicleEntity v = stringToVehicleEntity(line);
+                allVehicle.put(v.registrationNumber, v);
             }
+            br.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-
+    private VehicleEntity stringToVehicleEntity(String line) {
+        String[] vehicleStr = line.split(",");
+        VehicleEntity v = new VehicleEntity();
+        v.registrationNumber = (Objects.equals(vehicleStr[0], "") ? null : vehicleStr[0]);
+        v.vehicleRegister = (Objects.equals(vehicleStr[1], "") ? null : vehicleStr[1]);
+        v.vehicle = (Objects.equals(vehicleStr[2], "") ? null : vehicleStr[2]);
+        v.make = (Objects.equals(vehicleStr[3], "") ? null : vehicleStr[3]);
+        v.model = (Objects.equals(vehicleStr[4], "") ? null : vehicleStr[4]);
+        v.numberOfSeats = Integer.parseInt(vehicleStr[5]);
+        v.vehicleType = (Objects.equals(vehicleStr[6], "") ? null : vehicleStr[6]);
+        return v;
+    }
     private String vehicleEntityToString(VehicleEntity v) {
         return (v.registrationNumber == null ? "" : v.registrationNumber ) +
                 "," + (v.vehicleRegister == null ? "" : v.vehicleRegister ) +
--- src/test/java/SaveFileTest.java
+++ src/test/java/SaveFileTest.java
@@ -1,10 +1,32 @@
-import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorage;
+import hu.kovacspeterzoltan.bootcamp.vehiclepersistencestorage.PersistenceStorageCSV;
 import hu.kovacspeterzoltan.bootcamp.vehicleregister.VehicleEntity;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class SaveFileTest {
     @Test
     void testSaveFile() {
+        VehicleEntity ve = getVehicleEntity();
+
+        PersistenceStorageCSV storage = new PersistenceStorageCSV();
+        storage.saveVehicle(ve);
+    }
+    @Test
+    void testGetNullIfNotExists() {
+        VehicleEntity ve = getVehicleEntity();
+        PersistenceStorageCSV storage = new PersistenceStorageCSV();
+        storage.saveVehicle(ve);
+        Assertions.assertNull(storage.getVehicle("aaa"));
+    }
+    @Test
+    void testGetVehicleIfExists() {
+        VehicleEntity ve = getVehicleEntity();
+        PersistenceStorageCSV storage = new PersistenceStorageCSV();
+        storage.saveVehicle(ve);
+        Assertions.assertEquals(ve.registrationNumber, storage.getVehicle(ve.registrationNumber).registrationNumber);
+    }
+
+    private static VehicleEntity getVehicleEntity() {
         VehicleEntity ve = new VehicleEntity();
         ve.registrationNumber = "regNum";
         ve.vehicleRegister = "vehicleRegister";
@@ -13,8 +35,6 @@ public class SaveFileTest {
         ve.model = "model";
         ve.numberOfSeats = 5;
         ve.vehicleType = "vehicleType";
-
-        PersistenceStorage storage = new PersistenceStorage();
-        storage.saveVehicle(ve);
+        return ve;
     }
 }
